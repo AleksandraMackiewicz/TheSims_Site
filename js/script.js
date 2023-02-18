2ea71cd44141dec24c13802fcaa1684a1b7a7627
= <=>
2023-02-18 15:48:34 +0100

    feat: add js

--- js/script.js
+++ js/script.js
@@ -1,9 +1,21 @@
 /* eslint-disable linebreak-style */
 console.log('I work');
 const menuBtn = document.querySelector('.hamburger');
-const mobileNav = document.querySelector('.mobile_nav')
+const mobileNav = document.querySelector('.mobile_nav');
 
 menuBtn.addEventListener('click', () => {
   menuBtn.classList.toggle('is_active');
   mobileNav.classList.toggle('is_active');
 });
+
+// button take me to other subsite
+const moreSpellsButton = document.querySelector('#moreSpellsButton').onclick = function() {
+  window.location.href = "/mod/More_spells.html";
+};
+const beyondLifeButton = document.querySelector('#beyondLifeButton').onclick = function() {
+  window.location.href = "/mod/Beyond_life.html";
+};
+
+const homeButton = document.querySelector('#home').onclick = function() {
+  window.location.href = "/index.html"
+}
---
+++ mod/script.js
@@ -0,0 +1,13 @@
+/* eslint-disable linebreak-style */
+console.log('I work');
+const menuBtn = document.querySelector('.hamburger');
+const mobileNav = document.querySelector('.mobile_nav');
+
+menuBtn.addEventListener('click', () => {
+  menuBtn.classList.toggle('is_active');
+  mobileNav.classList.toggle('is_active');
+});
+
+const homeButton = document.querySelector('#home').onclick = function() {
+  window.location.href = "/index.html"
+}
